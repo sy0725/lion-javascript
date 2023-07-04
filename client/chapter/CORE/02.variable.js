6171811fa0aa3575d8e66554ed45293b853257be
sy0725 <sunyong0725@naver.com>
2023-07-04 15:48:52 +0900

    0704 수업 내용 업데이트

---
+++ client/chapter/CORE/02.variable.js
@@ -0,0 +1,65 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+// - Calculate product price by quantity  === calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+//  - totalProductPrice
+
+// - 1년 기준 일(day)자 수
+// DAYS_PER_YEAR
+
+// - 구매 결제 여부
+// isPayment
+
+// - 구매 결제 내역
+// paymentHistory
+
+// - 브랜드 접두사
+// BRAND_PREFIX
+
+// - 오늘의 운세
+// fortuneOfToday
+
+// - 상품 정보
+ // productInformation
+
+/* variables ----------------------------------------------------------- */
+
+// - 오늘의 운세 O
+// - 구매 결제 여부 o
+// - 구매 제품 가격의 총 합 o
+// - 갯수 별 상품 가격 계산하기 o
+// - 구매 결제 내역 x
+
+
+let todayLucky;
+
+let buyThis;
+
+let buyItemTotalPrice;
+
+let calcProductPriceQuantity;
+
+
+
+/* constant variables -------------------------------------------------- */
+
+// - 상품 정보 O
+// - 브랜드 접두사 o
+// - 1년 기준 일(day)자 수 O
+
+const itemInformation;
+
+const BRAND_NAME;
+
+const itemReceipt;
+
+const DAYS_PER_YEAR;
+
+

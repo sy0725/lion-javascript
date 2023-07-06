349d07b9c3f2f15669f6c4d15dd1728a58033d07
sy0725 <sunyong0725@naver.com>
2023-07-06 11:23:51 +0900

    08-2.condition.js 추가

---
+++ client/chapter/CORE/08-2.condition.js
@@ -0,0 +1,32 @@
+/* ------------------- */
+/* Logical Operators   */
+/* ------------------- */
+
+let a = 10;
+let b = '';
+let value = Boolean(b);
+
+// 논리곱(그리고) 연산자
+let AandB = a && b;
+
+// 논리합(또는) 연산자
+let AorB = a || b;
+
+// 부정 연산자
+let reverseValue = !value;
+
+
+// 조건 처리
+
+// 첫번째 Falsy를 찾는 연산 (&&)
+let whichFalsy = true && ' ' && [] && {thisIsFalsy: false};
+
+// 맨 마지막
+
+// 첫번째 Truthy를 찾는 연산 (||)
+let whichTruthy = false || '' || [2,3].length || {thisIsTruthy:true};
+
+// 2
+
+//[2,3].length = 배열의 개수를 알려주는거  = 2;
+
